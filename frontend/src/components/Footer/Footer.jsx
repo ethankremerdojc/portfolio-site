573bcefe49dfa27bc46d829f18b5dbf2a02c6639
Ethan <ethankremerdojc@gmail.com>
2026-09-15 11:33:51 -0600

    update header, footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,19 @@
-export default function Footr() {
+import './Footer.css';
+
+export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <div className="footer">
+      <div className="footer-inner">
+        <div className="footer-title">
+          <div className="footer-title-block">
+            <h3>Ethan Kremer</h3>
+          </div>
+        </div>
+
+        <div className="footer-contact-item">
+          <i>Copyright © 2026 Ethan Kremer. All Rights Reserved.</i>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,31 @@
+import './Header.css';
+
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <div className="header">
+      <div className="header-inner">
+        <div className="header-title">
+          <div className="header-icon">
+            ICON
+          </div>
+          <div className="header-title-block">
+            <h3>Ethan Kremer</h3>
+            <h4>Full stack &amp; Systems</h4>
+          </div>
+        </div>
+
+        <div className="header-nav">
+          <a href="#">Overview</a>
+          <a href="#">Experience</a>
+          <a href="#">Technologies</a>
+          <a href="#">Projects</a>
+          <a href="#">Console</a>
+        </div>
+
+        <div className="header-contact-item">
+          <a href="#">Get in touch</a>
+        </div>
+      </div>
+    </div>
   )
 }
